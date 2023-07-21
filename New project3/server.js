ecb8e5009e91a712ece9edb9777d4618a05f5615
dan <dankey2109@gmail.com>
2023-07-21 16:55:32 +0100

    end of the day

--- New project3/server.js
+++ New project3/server.js
@@ -9,8 +9,26 @@ let db=MySql.createConnection({
         database:"nbs2"
 });
         db.connect();
+
+        server.get("/employeesOFDepartment/:dept",function(req,res){
+            let sql1="select * from employees where department='"+req.params.dept+"'"
+            db.query(sql1,function(err,records){
+                res.render("employeeslist.ejs",{employees:records})
+                res.end()
+            })
+        })
+        server.get("/deleteEmployee/:empno",function(req,res){
+            let deleteSQL="delete from employees where empno=" +req.params.empno
+            db.query(deleteSQL,function(err,records){
+                
+                db.query("select * from employees",function(err,records){
+                    res.render("employeeslist.ejs",{employees:records})
+                    res.end()})
+                
+            })
+        })
     
-        server.get("/show",function(req,res){
+        server.get("/",function(req,res){
         res.render("homepage.ejs")
         res.end()
         })
@@ -20,9 +38,9 @@ let db=MySql.createConnection({
                 res.end()
             })
         })
-        server.get("/departments",function(req,res){
+        server.get("/department",function(req,res){
             db.query("select distinct department from employees",function(err,records){
-                res.render("departments.ejs.",{deptName:records})
+                res.render("departments.ejs",{deptName:records})
                 res.end()
             })
         })
