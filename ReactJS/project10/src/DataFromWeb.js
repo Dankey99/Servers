fc5b14cd5e75ec728d1d9eabf4ba269c6c774bfd
dankey <dankey2109@gmail.com>
2023-07-26 17:04:53 +0100

    emd of day

--- ReactJS/project10/src/DataFromWeb.js
+++ ReactJS/project10/src/DataFromWeb.js
@@ -15,6 +15,13 @@ function DataFromWeb(){
     let responsePromise = fetch(url)
     responsePromise.then(processResponse)
     }
+    function clickingButton2(){
+        let pid2=document.getElementById("t2").value
+        let url2= "https://jsonplaceholder.typicode.com/posts/"+pid2+"/comments"
+        let responsePromise2 = fetch(url2)
+        responsePromise2.then(processResponse)
+    }
+    
    
     return(
         <>
@@ -23,11 +30,10 @@ function DataFromWeb(){
         <input type="button" id="Go" onClick={clickingButton}></input>
         <table>
             {
-                records.map( (rec) =>
+                records.map((rec) =>
                 <tr>
-                    <td>{rec.name}</td>
-                    <td>{rec.email}</td>
-                    <td>{rec.body}</td>
+                    <td>{rec.name}<input type="button" id="t2" onClick={clickingButton2}></input></td>
+                    
                     
                 </tr>
                 )
